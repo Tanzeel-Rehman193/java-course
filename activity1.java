7083dd655751c69468056c9e31d6b07889cf63bd
Tanzeel-Rehman193 <www.tanzil.u.rehman@gmail.com>
2025-05-16 21:46:43 +0500

    first activity
    first activity using java

---
+++ activity1.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class activity1 {
+    public static void main(String[]args){
+        int num1 ,num2;
+        double sale;
+        char first;
+        num1 = 4;
+        System.out.println("Value of num1 is :"+num1);
+        num2 = 4 * 5 -11;
+        System.out.println("Value of num2 is :"+num2);
+        sale = 0.02 * 1000;
+        System.out.println("sale is :"+sale);
+        first = 'D';
+        System.out.println("first:"+first);
+    }
+}
